4428a1983108947c6dc71a1df7ca03e01c42bfc8
Norbaev <timnrbv@gmail.com>
2024-05-31 22:03:21 +0500

    create module loader

---
+++ loader/loader.js
@@ -0,0 +1,100 @@
+function createCircle({ size, halfSize, radius, strokeWidth, color }) {
+  const circle = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'circle'
+  );
+
+  const attribute = {
+    cx: String(halfSize),
+    cy: String(halfSize),
+    r: String(radius),
+    fill: 'transparent',
+    stroke: color,
+    'stroke-width': String(strokeWidth),
+    transform: `translate(0, ${size}) rotate(-90)`,
+  };
+
+  for (const key in attribute) {
+    circle.setAttribute(key, attribute[key]);
+  }
+
+  return circle;
+}
+
+export function createLoader({
+  size = 100,
+  strokeWidth = 10,
+  value = 100,
+  animationSpeed = 3000,
+  transitionSpeed = 1000,
+  className = '',
+} = {}) {
+  const container = document.createElement('div');
+  container.classList.add('lib-loader');
+  if (className) container.classList.add(className);
+  container.style.width = `${size}px`;
+  container.style.height = `${size}px`;
+
+  const halfSize = size / 2;
+  const radius = halfSize - strokeWidth / 2;
+  const circleLength = 2 * Math.PI * radius;
+
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('height', String(size));
+  svg.setAttribute('width', String(size));
+
+  const circleGray = createCircle({
+    size,
+    halfSize,
+    radius,
+    strokeWidth,
+    color: '#eaf0f6',
+  });
+
+  const circle = createCircle({
+    size,
+    halfSize,
+    radius,
+    strokeWidth,
+    color: '#324ff9',
+  });
+
+  svg.append(circleGray);
+  svg.append(circle);
+  container.append(svg);
+
+  container.setIsHidden = (isHidden) => {
+    container.setAttribute('style', `visibility: ${isHidden ? 'hidden' : ''}`);
+  };
+  container.setIsAnimated = (isAnimated) => {
+    svg.setAttribute(
+      'style',
+      `animation: ${
+        isAnimated ? `loader-rotation linear ${animationSpeed}ms infinite` : ''
+      }`
+    );
+  };
+  container.setValue = (value) => {
+    if (value > 100) value = 100;
+    if (value < 0) value = 0;
+
+    const valuePercent = (100 - value) / 100;
+
+    circle.setAttribute(
+      'style',
+      `
+          stroke-dasharray: ${circleLength};
+          stroke-dashoffset: ${circleLength * valuePercent};
+          transition: stroke-dashoffset ${transitionSpeed}ms;
+        `
+    );
+  };
+  container.setValue(value);
+
+  return container;
+}
+
+const link = document.createElement('link');
+link.setAttribute('href', '/loader/loader.css');
+link.setAttribute('rel', 'stylesheet');
+document.head.append(link);
